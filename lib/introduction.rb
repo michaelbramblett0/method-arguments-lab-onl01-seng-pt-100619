3b49c3c766bfc35c93af1331f390b95d6248d506
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-10-13 00:10:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
+introduction ("michael")
 
 def introduction_with_language(name,language)
   puts "Hi, my names is #{name} and I am learning to program in #{language}."
